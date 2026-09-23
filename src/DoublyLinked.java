c3e5c18f0393af39e831fba6789170d32be8dba6
spacepiratetodd-a11y <spacepiratetodd@gmail.com>
2026-09-23 10:42:29 -0600

    empty?

--- src/DoublyLinked.java
+++ src/DoublyLinked.java
@@ -1,5 +1,78 @@
+public class DoublyLinkedList<E> {
 
-public class DoublyLinked
-{
+    private static class DNode<E> {
+        E data;
+        DNode<E> next;
+        DNode<E> prev;
 
+        DNode(E value) {
+            this.data = value;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    private DNode<E> head;
+    private DNode<E> tail;
+    private int size;
+
+    public void add(E value) {
+        DNode<E> newNode = new DNode<>(value);
+
+        if (head == null) {
+            head = tail = newNode;
+        } else {
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        }
+
+        size++;
+    }
+
+    public E popFront() {
+        if (head == null) return null;
+
+        E value = head.data;
+
+        if (head == tail) {
+            head = tail = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+
+        size--;
+        return value;
+    }
+
+    public E popEnd() {
+        if (tail == null) return null;
+
+        E value = tail.data;
+
+        if (head == tail) {
+            head = tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+        }
+
+        size--;
+        return value;
+    }
+
+    public String toString() {
+        StringBuilder sb = new StringBuilder("[");
+        DNode<E> current = head;
+
+        while (current != null) {
+            sb.append(current.data);
+            if (current.next != null) sb.append(", ");
+            current = current.next;
+        }
+
+        sb.append("]");
+        return sb.toString();
+    }
 }
